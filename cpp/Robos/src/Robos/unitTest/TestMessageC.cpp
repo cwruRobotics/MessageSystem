a261ebcd81763aabff442753669263a4814f692d
Andrew Wood <aew61@case.edu>
2016-03-26 16:28:23 -0400

    more Robos testing
    Added testing for a cycle of nodes

---
+++ cpp/Robos/src/Robos/unitTest/TestMessageC.cpp
@@ -0,0 +1,21 @@
+// SYSTEM INCLUDES
+
+
+// C++ PROJECT INCLUDES
+#include "Robos/unitTest/TestMessageC.hpp"
+
+namespace Robos
+{
+namespace Tests
+{
+
+    TestMessageC::TestMessageC() : MessageBase("testTopicC")
+    {
+    }
+
+    TestMessageC::~TestMessageC()
+    {
+    }
+
+} // end of namespace Tests
+} // end of namespace Robos

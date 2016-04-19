31673aea036bd9fa8eac9388782d6387e01c6412
Andrew Wood <aew61@case.edu>
2016-04-19 10:07:19 -0400

    last change to Robos_FT using Python
    Still need to check Unix but some comments to clear up the process.

--- cpp/Robos_FT/Robos_FT_Python_Client/src/Nodes/PyFactorialNode.cpp
+++ cpp/Robos_FT/Robos_FT_Python_Client/src/Nodes/PyFactorialNode.cpp
@@ -22,7 +22,10 @@ namespace Robos_FT_Cpp_Client
 
     Robos::MessageBasePtr PyFactorialNode::UnpackPythonObject(PyObject* pObject)
     {
+        // we expect Python's result to be an int.
         int result = PyInt_AsLong(pObject);
+
+        // package up everything into a FactorialMessage
         FactorialMessagePtr pMessage = std::make_shared<FactorialMessage>(this->_factorialArg, result);
         return pMessage;
     }
@@ -31,9 +34,16 @@ namespace Robos_FT_Cpp_Client
     {
         if(pMessage->topic == "SensorMessage")
         {
+            // cast Message to appropriate type
             auto pCasted = std::dynamic_pointer_cast<SensorMessage>(pMessage);
+
+            // save factorial arg for later (will stuff into FactorialMessage later)
             this->_factorialArg = pCasted->numToFactorial;
+
+            // create a new Python tuple
             PyObject* pArgs = PyTuple_New(1);
+
+            // fill the tuple with the value we want to give python.
             PyTuple_SetItem(pArgs, 0, PyInt_FromLong(pCasted->numToFactorial));
             return pArgs;
         }
